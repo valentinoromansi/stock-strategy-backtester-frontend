92dd61ab5715b4ef147b57427b061e76f2e97518
valentinoromansi <valentinoromansi@gmail.com>
2022-10-23 01:00:17 +0200

    night mode for report table, css changed

--- src/components/strategy-report-table/strategy-report-columns.tsx
+++ src/components/strategy-report-table/strategy-report-columns.tsx
@@ -1,27 +1,32 @@
 import { ColumnsType } from "antd/lib/table";
 import { BacktestResult, TradeDateAndValues } from "../../models/backtest-result";
 
+const color = {
+  green: 'limegreen',
+  red: 'red',
+  neutral: '#9e9e9e'
+}
 
 let getColorFromPlFactor = (plFactor: any) : string => {
   if (plFactor < 0.5)
-    return 'red'
+    return color.red
   else if (plFactor === 0.5)
-    return 'black'
-  return 'limegreen'
+    return color.neutral
+  return color.green
 } 
 let getColorFromPlRatio = (plRatio: any) : string => {
   if (plRatio < 1)
-    return 'red'
+    return color.red
   else if (plRatio === 1)
-    return 'black'
-  return 'limegreen'
+    return color.neutral
+  return color.green
 }
 let getColorFromAvgProfit = (avgProfit: any) : string => {
   if (avgProfit < 0)
-    return 'red'
+    return color.red
   else if (avgProfit === 0)
-    return 'black'
-  return 'limegreen'
+    return color.neutral
+  return color.green
 }
 
 let getLastXTrades = (record: BacktestResult, numOfTrades: number): {profitTrades: TradeDateAndValues[], lossTrades: TradeDateAndValues[]} => {
@@ -104,11 +109,11 @@ export const columns: ColumnsType<BacktestResult> = [
       const sample = record.timesProfited + record.timesLost + record.timesIndecisive
       return <span>
         <span>{sample} (</span>
-        <span style={{ color: 'limegreen' }}>{( record.timesProfited / sample * 100 || 0).toFixed(2)}%</span>
+        <span style={{ color: color.green }}>{( record.timesProfited / sample * 100 || 0).toFixed(2)}%</span>
         <span> - </span>
-        <span style={{ color: 'red' }}>{( record.timesLost / sample * 100 || 0).toFixed(2)}%</span>
+        <span style={{ color: color.red }}>{( record.timesLost / sample * 100 || 0).toFixed(2)}%</span>
         <span> - </span>
-        <span style={{ color: 'lightgrey' }}>{( record.timesIndecisive / sample * 100 || 0).toFixed(2)}%</span>
+        <span style={{ color: color.neutral }}>{( record.timesIndecisive / sample * 100 || 0).toFixed(2)}%</span>
         <span>)</span>
       </span>
     })
@@ -132,9 +137,9 @@ export const columns: ColumnsType<BacktestResult> = [
       const trades = getLastXTrades(record, 30)
       const tradesSum = trades.profitTrades.length + trades.lossTrades.length
       return <span>
-        <span style={{ color: 'limegreen' }}>{(trades.profitTrades.length / tradesSum * 100 || 0).toFixed(2) + '%'}</span>
+        <span style={{ color: color.green }}>{(trades.profitTrades.length / tradesSum * 100 || 0).toFixed(2) + '%'}</span>
         <span> - </span>
-        <span style={{ color: 'red' }}>{(trades.lossTrades.length / tradesSum * 100 || 0).toFixed(2) + '%'}</span>
+        <span style={{ color: color.red }}>{(trades.lossTrades.length / tradesSum * 100 || 0).toFixed(2) + '%'}</span>
       </span>
     })
   }
